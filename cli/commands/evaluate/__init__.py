7dfeb1d3937fe359ac4e9bf841ed63a68104735d
Tim Ou <x0928048316@gmail.com>
2025-05-29 17:07:07 +0800

    feat: add eval subcommands (liquidation_accounting_error, liquidation_dos, and liquidation_small_position)

--- cli/commands/evaluate/__init__.py
+++ cli/commands/evaluate/__init__.py
@@ -4,6 +4,9 @@ import typer
 
 from . import eval as evalutation_tool
 from . import eval_reentrancy
+from . import eval_liquidation_accounting_error
+from . import eval_liquidation_dos
+from . import eval_liquidation_small_position
 
 app = typer.Typer()
 
@@ -50,5 +53,74 @@ def reentrancy(
     ),
 ):
     eval_reentrancy.evaluate_reentrancy(
+        csv_path, source_code_path, n8n_workflow_webhook_url
+    )
+
+
+@app.command()
+def liquidation_accounting_error(
+    csv_path: str = typer.Option(
+        "./dataset/liquidation_accounting_error.csv",
+        "--csv-path",
+        help="The path to the csv file to estimate",
+    ),
+    source_code_path: str = typer.Option(
+        "./dataset/liquidation_accounting_error/",
+        "--source-code-path",
+        help="The path to the source code directory",
+    ),
+    n8n_workflow_webhook_url: str = typer.Option(
+        "http://localhost:5678/webhook/liquidation_accounting_error",
+        "--n8n-workflow-webhook-url",
+        help="The URL of the n8n workflow webhook to test",
+    ),
+):
+    eval_liquidation_accounting_error.evaluate_liquidation_accounting_error(
+        csv_path, source_code_path, n8n_workflow_webhook_url
+    )
+
+
+@app.command()
+def liquidation_dos(
+    csv_path: str = typer.Option(
+        "./dataset/liquidation_dos.csv",
+        "--csv-path",
+        help="The path to the csv file to estimate",
+    ),
+    source_code_path: str = typer.Option(
+        "./dataset/liquidation_dos/",
+        "--source-code-path",
+        help="The path to the source code directory",
+    ),
+    n8n_workflow_webhook_url: str = typer.Option(
+        "http://localhost:5678/webhook/liquidation_dos",
+        "--n8n-workflow-webhook-url",
+        help="The URL of the n8n workflow webhook to test",
+    ),
+):
+    eval_liquidation_dos.evaluate_liquidation_dos(
+        csv_path, source_code_path, n8n_workflow_webhook_url
+    )
+
+
+@app.command()
+def liquidation_small_position(
+    csv_path: str = typer.Option(
+        "./dataset/liquidation_small_position.csv",
+        "--csv-path",
+        help="The path to the csv file to estimate",
+    ),
+    source_code_path: str = typer.Option(
+        "./dataset/liquidation_small_position/",
+        "--source-code-path",
+        help="The path to the source code directory",
+    ),
+    n8n_workflow_webhook_url: str = typer.Option(
+        "http://localhost:5678/webhook/liquidation_small_position",
+        "--n8n-workflow-webhook-url",
+        help="The URL of the n8n workflow webhook to test",
+    ),
+):
+    eval_liquidation_small_position.evaluate_liquidation_small_position(
         csv_path, source_code_path, n8n_workflow_webhook_url
     )
